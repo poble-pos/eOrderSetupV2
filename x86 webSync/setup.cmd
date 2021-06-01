a5ac78a25c8e8731a48c72bfeb3f1c447d6c194c
Young Jung <young.jung@poble.com.au>
2021-06-01 13:08:54 +1000

    order by class , menu name, healthcheck, auto script for webSync

---
+++ x86 webSync/setup.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+rem #############################
+rem ##                         ##
+rem ##  Written by: David Shin ##
+rem ##                         ##
+rem #############################
+
+sqlcmd -S .\REST -d eRES15 -Q "ALTER DATABASE eRES15 SET ENABLE_BROKER WITH ROLLBACK IMMEDIATE;"
+sqlcmd -S .\REST -d eRES15 -Q "EXEC sp_changedbowner 'sa'"
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE dbo.BestSeller ADD BestSellerID int not null PRIMARY KEY IDENTITY(1, 1)"
+
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE MenuItem ALTER COLUMN LargeMenuImage VARBINARY(MAX)"
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE MenuItem ALTER COLUMN MenuImage VARBINARY(MAX)"
+
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE StoreSetting ALTER COLUMN StoreImage VARBINARY(MAX)"
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE StoreSetting ALTER COLUMN ADImage VARBINARY(MAX)"
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE StoreSetting ALTER COLUMN DualImage VARBINARY(MAX)"
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE StoreSetting ALTER COLUMN Printer13COMIP VARCHAR (20)"
+
+
+sqlcmd -S .\REST -d eRES15 -Q "ALTER TABLE MenuModifier ALTER COLUMN ModifierImage VARBINARY(MAX)"
+
+echo.
+pause
